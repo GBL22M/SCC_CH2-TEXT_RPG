600efcb0d31788b0fc18ac3a261a78e2dc95fe7a
burperring <khw5231@gmail.com>
2025-01-13 15:12:04 +0900

    Change Battle System

--- CH2_RPG/GameManager.h
+++ CH2_RPG/GameManager.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "Character.h"
 #include "Monster.h"
+#include "RandomIntGenerator.h"
+#include "Item.h"
+#include "HealthPotion.h"
+#include "AttackBoost.h"
 
 class GameManager
 {
@@ -8,7 +12,11 @@ public:
 	Monster* GenerateMonster(int level);
 	bool Battle(Character* player);
 	void DisplayInventory(Character* player);
-	
+
 private:
 	const int mTotalMonsterCount = 3;
 };
+
+bool MonsterDeadCheck(Character* player, Monster* monster);
+
+bool PlayerDeadCheck(Character* player);
